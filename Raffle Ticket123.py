002f2f43cfdb818da8f26577628d802329999cb8
wni12533-del <wni12533@gmail.com>
2026-02-15 09:51:45 +1300

    Raffle Ticket
    I have done some error catching and just made the code more appealing and aesthetic, (I added a print ("-* * 30))

--- Raffle Ticket123.py
+++ Raffle Ticket123.py
@@ -2,20 +2,47 @@
 
 import random
 
+import time
+
 names = list()
 
 print("Welcome to the raffle program")
 
 raffle_prize = input("What is the prize for the raffle? > ")
 
-raffle_cost = int(input(f"What is the cost of the {raffle_prize} (do not enter the $ sign)> "))
+raffle_cost_input = True
+while raffle_cost_input:
+    try:
+        raffle_cost = int(input(f"What is the cost of the {raffle_prize} (do not enter the $ sign)> "))
+        if raffle_cost < 0:
+            print("Invalid cost. Please enter a positive integer.")
+        else:
+            raffle_cost_input = False
+    except ValueError: 
+        print("Invalid. Please enter an integer")
 
-people = input("How many people are in the raffle? > ")
+people_input = True
+while people_input:
+    try:
+        people = int(input("How many people are in the raffle? > "))
+        if people < 0:
+            print("Invalid number of people. Please enter a positive integer.")
+        else:
+            people_input = False
+    except ValueError: 
+        print("Invalid. Please enter an integer")
+
+
 
 for i in range(1, int(people) + 1):
     new_name = input("Enter name of person " + str(i) + ": ")
     names.append(new_name)
 
 winner = random.choice(names)
+input("Press Enter to continue...")
+print("-" * 30)
+print("Calculating the winner...")
+
+time.sleep(2)
 
 print(f"The winner of the {raffle_prize} that costs ${raffle_cost} is person number {names.index(winner) + 1} {winner}!")
---
+++ RandomLetter.py
@@ -0,0 +1,6 @@
+'''Random Letter Generator'''
+
+import random
+print("Welcome to the random letter generator!")
+letter = random.choice("abcdefghijklmnopqrstuvwxyz")
+print(f"The random letter generated is: {letter}")
